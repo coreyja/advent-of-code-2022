f3610ee7465851a7516e92a40d98f431f5217940
Corey Alexander <coreyja@gmail.com>
2022-12-05 10:29:43 -0500

    Day 2 Part 2

--- 02-rock-paper-scissors/src/main.rs
+++ 02-rock-paper-scissors/src/main.rs
@@ -5,11 +5,28 @@ struct Round {
 }
 
 impl Round {
-    fn parse(input: &str) -> Self {
-        let mut moves = input.split(' ').map(Move::parse);
+    fn parse_part1(input: &str) -> Self {
+        let mut moves = input.split(' ').map(Move::parse_part1);
 
         let opponent_move = moves.next().unwrap();
         let your_move = moves.next().unwrap();
+
+        Round {
+            opponent_move,
+            your_move,
+        }
+    }
+
+    fn parse_part2(input: &str) -> Self {
+        let mut split = input.split(' ');
+
+        let opponent_move = split.next().unwrap();
+        let opponent_move = Move::parse_part1(opponent_move);
+
+        let end_result = split.next().unwrap();
+        let end_result = Result::parse(end_result);
+
+        let your_move = opponent_move.your_move_for_result(end_result);
 
         Round {
             opponent_move,
@@ -33,8 +50,25 @@ enum Move {
     Scissors,
 }
 
-impl Move {
+enum Result {
+    Win,
+    Lose,
+    Draw,
+}
+
+impl Result {
     fn parse(input: &str) -> Self {
+        match input {
+            "X" => Result::Lose,
+            "Y" => Result::Draw,
+            "Z" => Result::Win,
+            _ => panic!("Unknown result character"),
+        }
+    }
+}
+
+impl Move {
+    fn parse_part1(input: &str) -> Self {
         match input {
             "A" => Move::Rock,
             "B" => Move::Paper,
@@ -67,10 +101,34 @@ impl Move {
             (Move::Paper, Move::Paper) => 3,
         }
     }
+
+    /// This method takes in the intended result, and returns the move you
+    /// have to play to get the Result in the round
+    fn your_move_for_result(&self, intended_result: Result) -> Self {
+        match (self, intended_result) {
+            (Move::Rock, Result::Draw) => Move::Rock,
+            (Move::Rock, Result::Win) => Move::Paper,
+            (Move::Rock, Result::Lose) => Move::Scissors,
+
+            (Move::Paper, Result::Draw) => Move::Paper,
+            (Move::Paper, Result::Win) => Move::Scissors,
+            (Move::Paper, Result::Lose) => Move::Rock,
+
+            (Move::Scissors, Result::Draw) => Move::Scissors,
+            (Move::Scissors, Result::Win) => Move::Rock,
+            (Move::Scissors, Result::Lose) => Move::Paper,
+        }
+    }
 }
 
 fn part_1(input: &str) -> u64 {
-    let rounds: Vec<Round> = input.lines().map(Round::parse).collect();
+    let rounds: Vec<Round> = input.lines().map(Round::parse_part1).collect();
+
+    rounds.iter().map(|r| r.score()).sum()
+}
+
+fn part_2(input: &str) -> u64 {
+    let rounds: Vec<Round> = input.lines().map(Round::parse_part2).collect();
 
     rounds.iter().map(|r| r.score()).sum()
 }
@@ -79,8 +137,10 @@ fn main() {
     let my_input = include_str!("my.input");
 
     let example_part1_ans = part_1(example_input);
-    dbg!(example_part1_ans);
+    let example_part2_ans = part_2(example_input);
+    dbg!(example_part1_ans, example_part2_ans);
 
     let my_part1_ans = part_1(my_input);
-    dbg!(my_part1_ans);
+    let my_part2_ans = part_2(my_input);
+    dbg!(my_part1_ans, my_part2_ans);
 }

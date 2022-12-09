4a5d6780e0ff87ae1ccce89f47b134d33e5d5fa9
Corey Alexander <coreyja@gmail.com>
2022-12-08 22:41:40 -0500

    Day 6 Part 2
    This time we made the same function before generic over the length, and part 2 was solved!

--- 06-tuning-trouble/src/lib.rs
+++ 06-tuning-trouble/src/lib.rs
@@ -1,9 +1,9 @@
 use std::collections::HashSet;
 
-pub fn part_1(input: &str) -> usize {
+pub fn end_of_packet_indicator<const WINDOW_SIZE: usize>(input: &str) -> usize {
     let chars: Vec<_> = input.chars().collect();
     chars
-        .windows(4)
+        .windows(WINDOW_SIZE)
         .enumerate()
         .find(|(_, window)| {
             let hash_set: HashSet<_> = window.iter().collect();
@@ -12,7 +12,7 @@ pub fn part_1(input: &str) -> usize {
         })
         .unwrap()
         .0
-        + 4
+        + WINDOW_SIZE
 }
 
 #[cfg(test)]
@@ -22,7 +22,7 @@ mod tests {
     #[test]
     fn example_1_input_part_1() {
         let input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
-        let ans = part_1(input);
+        let ans = end_of_packet_indicator::<4>(input);
 
         assert_eq!(ans, 7);
     }
@@ -30,8 +30,24 @@ mod tests {
     #[test]
     fn my_input_part_1() {
         let input = include_str!("my.input");
-        let ans = part_1(input);
+        let ans = end_of_packet_indicator::<4>(input);
 
         assert_eq!(ans, 1531);
+    }
+
+    #[test]
+    fn example_1_input_part_2() {
+        let input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+        let ans = end_of_packet_indicator::<14>(input);
+
+        assert_eq!(ans, 19);
+    }
+
+    #[test]
+    fn my_input_part_2() {
+        let input = include_str!("my.input");
+        let ans = end_of_packet_indicator::<14>(input);
+
+        assert_eq!(ans, 2518);
     }
 }
